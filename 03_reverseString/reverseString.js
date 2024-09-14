b65799b5d1f8147bbd1e26022bd450301d62ab31
freelogan7 <163227178+FreeLogan7@users.noreply.github.com>
2024-09-14 16:34:33 -0400

    Exercise 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+let reverseword ="";
+    for (let i = word.length-1; i >= 0;  i--){
+        reverseword += word[i];
+    }
+    return reverseword;
 };
 
-// Do not edit below this line
 module.exports = reverseString;
