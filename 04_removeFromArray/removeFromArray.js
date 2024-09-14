f95e290872768fd1891c838d25d4420c7a961bd3
freelogan7 <163227178+FreeLogan7@users.noreply.github.com>
2024-09-14 18:27:17 -0400

    Created function that uses rest parameter -> Removes specified values from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+    const newArray = [];
+
+    //For each item in the array, copy to a new array
+    //if they don't match 'args' values
+    array.forEach(item => {
+
+        if(!args.includes(item)){
+            newArray.push(item);    
+        }
+    });
+    
+    return newArray
+   
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+//Simpler method: (filter function)
+// const removeFromArray = function(array, ...args) {
+// return array.filter(val => !args.includes(val))
