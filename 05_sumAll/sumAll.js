73c8ac79dece95bb97eebfa7f0e7696fcb73a22a
freelogan7 <163227178+FreeLogan7@users.noreply.github.com>
2024-09-15 20:32:44 -0400

    Solution failed on third test, works only if arguments are in the order of 'start' and 'end'

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    var sumAll =0;
+    for (var i = start; i <= end; i++){
+         sumAll += i;
+    };
+
+return sumAll;
 };
 
 // Do not edit below this line
