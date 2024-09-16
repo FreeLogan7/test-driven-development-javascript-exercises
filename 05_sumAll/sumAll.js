ca043f59adad4a4601298602e732540f3a83dc5b
freelogan7 <163227178+FreeLogan7@users.noreply.github.com>
2024-09-15 21:24:44 -0400

    added if to handle negative values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,19 @@
-const sumAll = function(start, end) {
-
+const sumAll = function(...args) {
     var sumAll =0;
-    for (var i = start; i <= end; i++){
+    var start = Math.min(...args);
+    var end = Math.max(...args);
+
+   if (start < 0) return "ERROR";
+
+
+
+    for (var i = start; i <= end; i++)
+         {
          sumAll += i;
-    };
+         }
 
 return sumAll;
-};
+}
 
 // Do not edit below this line
 module.exports = sumAll;
