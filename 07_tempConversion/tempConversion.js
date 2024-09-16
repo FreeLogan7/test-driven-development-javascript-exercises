24ca82d09decdccb1c2279c2c29f60d5e0b2040c
freelogan7 <163227178+FreeLogan7@users.noreply.github.com>
2024-09-16 16:45:07 -0400

    Completed exercise 07 through use of toFixed method, and Number conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+
+ 
+  const celsius = ((value - 32 ) * 5/9);
+
+  if (celsius % 1 == 0 || celsius == 0) return celsius;
+  else return Number(celsius.toFixed(1));
+
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+
+  const fahrenheit = ((value * 9/5) + 32);
+
+if (fahrenheit % 1 == 0 || fahrenheit == 0) return fahrenheit;
+  return Number(fahrenheit.toFixed(1));
+
 };
+
 
 // Do not edit below this line
 module.exports = {
