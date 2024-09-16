b6d57a3e1e5492679d9100a3693e1f3e6f8298ad
freelogan7 <163227178+FreeLogan7@users.noreply.github.com>
2024-09-15 21:39:46 -0400

    removed use of rest parameters due to issue with converting strings to integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,20 +1,24 @@
-const sumAll = function(...args) {
+const sumAll = function(start, end) {
     var sumAll =0;
-    var start = Math.min(...args);
-    var end = Math.max(...args);
-
-   if (start < 0) return "ERROR";
-   if (!Number.isInteger(start) || !Number.isInteger(end) ) return "ERROR";
-
+    
+    if (start > end) [start, end] = [end, start];
+     if (start < 0 || end < 0) return "ERROR";
+        if (!Number.isInteger(start) || !Number.isInteger(end) ) return "ERROR";
 
 
-    for (var i = start; i <= end; i++)
+
+        for (var i = start; i <= end; i++)
          {
          sumAll += i;
          }
 
-return sumAll;
+        return sumAll;
 }
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+// Couldn't use this method as it converts string numbers to integer
+// var start = Math.min(...args);
+// var end = Math.max(...args);
